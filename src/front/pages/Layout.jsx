9c892cd648f3fd50ff7b2b8ef21bf88744b11309
Xipxap11 <davidplanaquerol@gmail.com>
2026-08-07 19:56:13 +0000

    hacer el front del crud de clientes

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,13 +2,34 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
+import clientsServices from "../services/ClientsServices"
+import { useEffect } from "react"
+import useGlobalReducer from "../hooks/useGlobalReducer"
+import toast, { Toaster } from "react-hot-toast"
+
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
+    const { store, dispatch } = useGlobalReducer()
+
+    useEffect(() => {
+        async function getAllClients() {
+            try {
+                const dataClients = await clientsServices.getAllClientsByUser(store.user.id)
+                dispatch({ type: 'setClients', payload: dataClients })
+            } catch (error) {
+                toast.error("Characters didn't work.")
+            }
+        }
+        if (store.isAuthenticated) {
+            getAllClients()
+        }
+    }, []);
     return (
         <ScrollToTop>
+            <Toaster toastOptions={{style: { zIndex: 999 } }}/>
             <Navbar />
-                <Outlet />
+            <Outlet />
             <Footer />
         </ScrollToTop>
     )
